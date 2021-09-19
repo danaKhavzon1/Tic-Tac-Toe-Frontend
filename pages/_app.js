dbc4b44aef5a8cd204b61e81c1bdc44a994f4995
danakhavzon <danakhavzon@gmail.com>
2021-09-20 00:16:56 +0300

    changes supposed to make sure style loades

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,49 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import "../styles/globals.css";
 import PageLayout from "../components/PageLayout/PageLayout";
+
+const cacheURL = [];
+
+const handleLoadStyle = (url) => {
+  if (cacheURL.includes(url)) {
+    return;
+  }
+
+  const styleLinks = document.querySelectorAll(
+    'link[href*="/_next/static/css/styles.chunk.css"]'
+  );
+
+  const timestamp = new Date().valueOf();
+
+  [...styleLinks].map((link) => {
+    if (link.rel === "stylesheet") {
+      link.href = `/_next/static/css/styles.chunk.css?v=${timestamp}`;
+      cacheURL.push(url);
+    }
+  });
+};
+
 function MyApp({ Component, pageProps }) {
+  const { asPath, events } = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      return;
+    }
+
+    events.on("routeChangeComplete", handleLoadStyle);
+
+    return () => {
+      events.off("routeChangeComplete", handleLoadStyle);
+    };
+  }, [asPath]);
+
   return (
     <>
-        <PageLayout />
-        <Component {...pageProps} />
+      <PageLayout />
+      <Component {...pageProps} />
     </>
   );
 }
